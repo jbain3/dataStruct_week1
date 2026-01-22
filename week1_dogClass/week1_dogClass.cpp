57f54b79090a4eec22376724d1b0acf2abb85d62
Jordan Bain <jbain3@atu.edu>
2026-01-22 11:31:59 -0600

    added code

--- week1_dogClass/week1_dogClass.cpp
+++ week1_dogClass/week1_dogClass.cpp
@@ -1,6 +1,59 @@
 #include <iostream>
+#include <string.h>
+
+using namespace std;
+
+class Dog {
+private:
+    string name, breed;
+    int age;
+public:
+    void setName(string str);
+    void setAge(int num);
+    void setBreed(string str);
+    string getName();
+    int getAge();
+    string getBreed();
+    void bark();
+};
+
+void Dog::setName(string str) {
+    name = str;
+}
+
+void Dog::setAge(int num) {
+    age = num;
+}
+
+void Dog::setBreed(string str) {
+    breed = str;
+}
+
+string Dog::getName() {
+    return name;
+}
+
+int Dog::getAge() {
+    return age;
+}
+
+string Dog::getBreed() {
+    return breed;
+}
+
+void Dog::bark() {
+    cout << "Woof! Woof!" << endl;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Dog newDog;
+    newDog.setName("Star Destroyer");
+    newDog.setAge(5);
+    newDog.setBreed("Chihuahua");
+
+    cout << "Name: " << newDog.getName() << "\nAge: " << newDog.getAge() << "\nBreed: " << newDog.getBreed() << endl;
+
+    newDog.setBreed("Corgi");
+    newDog.bark();
 }
