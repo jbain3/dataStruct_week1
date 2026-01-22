5b808056b84f3b58ff3b0b972b2a799b8b705b14
Jordan Bain <jbain3@atu.edu>
2026-01-22 11:38:52 -0600

    added comments

--- week1_dogClass/week1_dogClass.cpp
+++ week1_dogClass/week1_dogClass.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+// Dog Class allows to get and set Name, Age, & Breed; also allows to bark.
 class Dog {
 private:
     string name, breed;
@@ -17,34 +18,42 @@ public:
     void bark();
 };
 
+// Sets Name to Parameter
 void Dog::setName(string str) {
     name = str;
 }
 
+// Sets Age to Parameter
 void Dog::setAge(int num) {
     age = num;
 }
 
+// Sets Bree to Parameter
 void Dog::setBreed(string str) {
     breed = str;
 }
 
+// Gets Name from Dog Class
 string Dog::getName() {
     return name;
 }
 
+// Gets Age from Dog Class
 int Dog::getAge() {
     return age;
 }
 
+// Gets Breed from Dog Class
 string Dog::getBreed() {
     return breed;
 }
 
+// Outputs Bark String
 void Dog::bark() {
     cout << "Woof! Woof!" << endl;
 }
 
+// Initializes Dog Class, gives it Name, Age, & Breed. Prints out all varaibles of dog class and changes breed before barking.
 int main()
 {
     Dog newDog;
